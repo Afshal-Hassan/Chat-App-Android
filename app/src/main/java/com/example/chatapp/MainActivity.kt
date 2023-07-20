95e4dbda09b0c3f568ece940d1e1e940f3409845
Afshal Hassan <afshal.hassan@venturedive.com>
2023-07-20 17:18:55 +0500

    Login and Home screen added

---
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.chatapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        onClickLetStartButton();
+    }
+
+
+    private fun onClickLetStartButton() {
+        val letStartButton = findViewById<Button>(R.id.letsStartedButton)
+        letStartButton.setClickListenerWithNavigation(::navigateToLogin)
+    }
+
+
+    private fun Button.setClickListenerWithNavigation(navigateFunction: () -> Unit) {
+        setOnClickListener {
+            navigateFunction.invoke()
+        }
+    }
+
+
+    private fun navigateToLogin(): Unit {
+        val loginIntent = Intent(this, Login::class.java);
+        startActivity(loginIntent);
+    }
+}
